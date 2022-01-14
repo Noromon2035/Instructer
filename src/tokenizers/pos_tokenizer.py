aeec8e56c821b7f6c4c4c050c0564d96842007d3
Noromon2035 <noronoro2035@gmail.com>
2022-01-14 19:56:46 +0800

    for generating pos tokens

--- src/tokenizers/pos_tokenizer.py
+++ src/tokenizers/pos_tokenizer.py
@@ -1,14 +1,17 @@
-import spacy
-from spacy.tokenizer import Tokenizer
-from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER, CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
-from spacy.util import compile_infix_regex
+try:
+    import spacy
+    from spacy.tokenizer import Tokenizer
+    from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER, CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
+    from spacy.util import compile_infix_regex
+except Exception as e:
+    print(e)
 
 def custom_tokenizer(nlp):
     infixes = (
         LIST_ELLIPSES
         + LIST_ICONS
         + [
-            r"(?<=[0-9])[+\-\*^](?=[0-9-])",
+            r"(?<=[0-9])[+\-\*^](?=[0-9-'])",
             r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
                 al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
             ),
@@ -19,12 +22,14 @@ def custom_tokenizer(nlp):
     )
 
     infix_re = compile_infix_regex(infixes)
+    exceptions=nlp.Defaults.tokenizer_exceptions
+    filtered_exceptions = {k:v for k,v in exceptions.items() if "'" not in k}
 
     return Tokenizer(nlp.vocab, prefix_search=nlp.tokenizer.prefix_search,
                                 suffix_search=nlp.tokenizer.suffix_search,
                                 infix_finditer=infix_re.finditer,
                                 token_match=nlp.tokenizer.token_match,
-                                rules=nlp.Defaults.tokenizer_exceptions)
+                                rules=filtered_exceptions)
 
 nlp = spacy.load("en_core_web_sm")
 nlp.tokenizer = custom_tokenizer(nlp)
