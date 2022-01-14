4280f26d1f67d2f4c430747a789a062e6fa07af0
Noromon2035 <noronoro2035@gmail.com>
2022-01-14 19:52:03 +0800

    For checking grammar v.1

--- src/checkers/grammar_checker_lt.py
+++ src/checkers/grammar_checker_lt.py
@@ -1,9 +1,10 @@
-print("Importing " + __file__)
-import language_tool_python
-print("Finished importing " + __file__)
+try:
+    import language_tool_python
+except Exception as e:
+    print(e)
+tool = language_tool_python.LanguageTool('en-US')
 
 def check(sentence):
-    tool = language_tool_python.LanguageTool('en-US')
     matches = tool.correct(sentence)
     print("The instructions was grammatically checked.\nResult = " + matches)
     return matches
