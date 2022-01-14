6455b98c9f71d911420fbcea936d5dd3fa1c3266
Noromon2035 <noronoro2035@gmail.com>
2022-01-14 20:00:50 +0800

    Instructer version 1.0

--- src/checkers/grammar_checker.py
+++
@@ -1,8 +0,0 @@
-#Grammarly lite
-from gingerit.gingerit import GingerIt
-
-def check(sentence):
-    parser = GingerIt()
-    result = parser.parse(sentence)
-    print("The instructions was grammatically checked.\nResult = " + result['result'])
-    return result['result']
